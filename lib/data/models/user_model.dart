d3bc05c87eb4ea93ceff5499fa844c13cf8601c0
RayanaRA <raanaraimzanova163@gmail.com>
2023-10-21 15:03:04 +0600

    example

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserModel extends Equatable {
+  final String? name;
+  final String? email;
+  final String? id;
+
+  UserModel({this.name, this.email, this.id});
+  // UserModel fromJson(){}
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'email': email,
+      'id': id,
+    };
+  }
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        email: json['email'],
+        id: json['id'],
+        name: json['name'],
+      );
+
+  @override
+  List<Object?> get props => [
+        name,
+        email,
+        id,
+      ];
+}
